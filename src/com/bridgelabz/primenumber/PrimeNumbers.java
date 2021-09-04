7ce211f98165a18353fdc4abf765c10cc35635c5
Swathi <swathi.h@perfios.com>
2021-09-04 07:56:50 +0530

    REFACTORED: Changed variable initialization

--- src/com/bridgelabz/primenumber/PrimeNumbers.java
+++ src/com/bridgelabz/primenumber/PrimeNumbers.java
@@ -11,21 +11,21 @@ public class PrimeNumbers {
 		ArrayList<Integer> arrayList = new ArrayList<Integer>();
 		for (int number = 2; number <= 1000; number++) {
 
-			boolean dimensionChange = true;
+			boolean dimensionChange = false;
 			for (int index = 2; index <= number / 2; index++) {
 				if (number % index == 0)
-					dimensionChange = false;
+					dimensionChange = true;
 			}
 
 			if (dimension < 99) {
-				if (dimensionChange)
+				if (!dimensionChange)
 					arrayList.add(number);
 				dimension++;
 			} else {
 				dimension = 0;
 				primeNumbers.add(arrayList);
 				arrayList = new ArrayList<Integer>();
-				if (dimensionChange)
+				if (!dimensionChange)
 					arrayList.add(number);
 				dimension++;
 			}
